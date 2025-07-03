c6e4b2813e464846f0ccb2682bb10dcd37fe1819
Yan921227 <125242772+Yan921227@users.noreply.github.com>
2025-07-03 15:39:32 +0800

    IMG_9672轉圖片

--- Video to picture.py
+++ Video to picture.py
@@ -36,7 +36,7 @@ def video_to_frames(video_path, output_folder, frame_interval=1):
     print(f"總共儲存 {saved_count} 張影格。")
 
 if __name__ == "__main__":
-    video_path = "C:\\Users\\User\\Desktop\\綜合揮拍姿勢影片\\IMG_9670.MOV" # 替換成你的影片路徑
-    output_folder = os.path.join(os.getcwd(), "frames", "IMG_9670")  # 指定巢狀資料夾
+    video_path = "C:\\Users\\User\\Desktop\\綜合揮拍姿勢影片\\IMG_9672.MOV" # 替換成你的影片路徑
+    output_folder = os.path.join(os.getcwd(), "frames", "IMG_9672")  # 指定巢狀資料夾
     frame_interval = 1
     video_to_frames(video_path, output_folder, frame_interval)
