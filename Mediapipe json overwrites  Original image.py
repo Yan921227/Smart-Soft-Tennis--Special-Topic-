2298e9ba12c3450eadbcf3bca4b15e2bf1f8390a
Yan921227 <125242772+Yan921227@users.noreply.github.com>
2025-07-04 22:07:21 +0800

    IMG_9671 json疊加圖片

--- Mediapipe json overwrites  Original image.py
+++ Mediapipe json overwrites  Original image.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     # 設定原始圖片所在的資料夾路徑
     image_folder = "D:\\Special topic data collection(2)\\frames\\IMG_9671"
     # 設定 JSON 標註檔所在的資料夾路徑
-    json_folder = "output_json\IMG_9670"
+    json_folder = "output_json\IMG_9671"
     # 設定輸出疊加後圖片的資料夾路徑（改為與 py 檔案同層）
     output_folder = os.path.join(os.getcwd(), "output_overlay", "IMG_9671")
     
